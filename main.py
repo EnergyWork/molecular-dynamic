eebf136f696052c39fa4d6d83a3f1d185c620db2
Artyom <ag.energy@mail.ru>
2021-04-26 01:37:02 +0300

    fix

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
+from progress.bar import ChargingBar
 
-
+large_motion = False
 N = 10
 DIM = 3
-SIZE = 5
-L_FREE_MOTION = np.power(np.power(SIZE, 3), 1/3.0) / (2*N)
-dt = 0.01
-SPEED = 2
+SIZE = 3
+L_FREE_MOTION = np.power(SIZE * SIZE * SIZE, 1/3.0) / (2*N)
+dt = 0.001
+SPEED = 1
 eps = 1
 sigma = 1
 rcut = 2.5 * sigma
@@ -21,6 +22,10 @@ m = np.array([np.zeros([DIM]) for _ in np.arange(N)])
 
 list_coords = np.array([])
 
+def clear_file():
+     with open('coords.xmol', 'w', encoding='utf-8') as file:
+        pass
+
 def new_frame(file_handle):
     file_handle.write(f'{N}\n')
 
@@ -30,14 +35,11 @@ def print_to_file(ti):
     global time
     with open('coords.xmol', 'a', encoding='utf-8') as file:
         new_frame(file)
-        file.write(f'***** time = * {time} *****')
+        file.write(f'***** time = * {time:.4} *****\n')
         for i in np.arange(N):
             file.write(f'Ar {r[i][0]:.4f} {r[i][1]:.4f} {r[i][2]:.4f} {v[i][0]:.4f} {v[i][1]:.4f} {v[i][2]:.4f}\n')
 
 def init_system():
-    global v
-    global r
-    global dr
     global m
     global list_coords
     for i in np.arange(N):
@@ -52,6 +54,8 @@ def init_system():
                 if counter < N:
                     r[counter] = np.array([(x + 1.0 / 2) * dh, (y + 1.0 / 2) * dh, (z + 1.0 / 2) * dh])
                     dr[counter] = np.array([ v[counter][0] * 2 * dt, v[counter][1] * 2 * dt, v[counter][2] * 2 * dt])
+                    if lenght(dr[counter]) > L_FREE_MOTION:
+                        print('init error')
                     counter += 1
     list_coords = np.append(list_coords, r)
     print_to_file(0)
@@ -75,9 +79,18 @@ def potential_LD(r):
 
 def verle_r(r, dr, f, m, dt): 
     return r + (dr + (f / (2 * m)) * np.square(dt))
+    #return 2*r-dr+(f/m)*np.square(dt)
 
 def verle_v(r, dr, dt):
     return dr / (2 * dt)
+    #return (r-dr)/(2*dt)
+
+def nim_fix(coord):
+    if coord >= SIZE / 2.0:
+        coord = SIZE - coord
+    elif coord <= -SIZE / 2.0:
+        coord = coord + SIZE
+    return coord
 
 # nearist image method
 def nim(r1, r2, size):
@@ -90,26 +103,15 @@ def nim(r1, r2, size):
     dist[1] = nim_fix(y)
     dist[2] = nim_fix(z)
 
-    return np.array([x, y, z]) #dist
-
-def nim_fix(coord):
-    if coord >= SIZE / 2.0:
-        coord = SIZE - coord
-    elif coord <= -SIZE / 2.0:
-        coord = coord + SIZE
-    return coord
+    return dist
 
 def lenght(r):
     return np.sqrt(np.sum([np.square(r[i]) for i in np.arange(np.size(r))]))
 
 def normalaize(vec):
-    return vec/lenght(vec)
+    return vec / lenght(vec)
 
 def calc_forces():
-    global v
-    global r
-    global v
-    global f
     for i in np.arange(N):
         f[i] = np.zeros(DIM)
         for j in np.arange(N):
@@ -117,12 +119,12 @@ def calc_forces():
                 rij = nim(r[i], r[j], SIZE)
                 _rij = lenght(rij)
                 ff = force_LD(_rij)
-                _dr = rij
+                _dr = rij.copy()
                 _dr = normalaize(_dr)
                 f[i] += _dr * ff
 
 def correct_coord(coord, left_boundary, right_boundary):
-    l = right_boundary - left_boundary
+    l = np.abs(right_boundary - left_boundary)
     d = 0
     if coord >= right_boundary:
         d = coord - left_boundary
@@ -132,40 +134,38 @@ def correct_coord(coord, left_boundary, right_boundary):
         coord = right_boundary - l * (d / l - np.floor(d / l))
     return coord
 
-#periodic  boundary condition
+#periodic boundary condition
 def pbc(r, size):
-    r[0] = correct_coord(r[0], 0, size)
-    r[1] = correct_coord(r[1], 0, size)
-    r[2] = correct_coord(r[2], 0, size)
+    return np.array([correct_coord(r[i], 0, size) for i in np.arange(r.size)]) 
 
 def integrate():
-    global v
-    global r
-    global dr
-    co = True
+    global large_motion
     for i in np.arange(N):
         r_tmp = r[i].copy()
         r[i] = verle_r(r[i], dr[i], f[i], m[i], dt)
         dr[i] = r[i] - r_tmp
-        if lenght(dr[i]) > L_FREE_MOTION:
-            co = False
+        if lenght(dr[i]) > L_FREE_MOTION :
+            if not large_motion:
+                large_motion = True
+                print(' - too large motion detected')
         v[i] = verle_v(r[i], dr[i], dt)
-        pbc(r[i], SIZE)
-    return co
+        r[i] = pbc(r[i], SIZE)
 
 def main():
     global list_coords
     global time
-    global dt
-    steps = 100
+    steps = 1000
+    clear_file()
     init_system()
-    for i in np.arange(1, steps+1):
-        calc_forces()
-        integrate()
-        list_coords = np.append(list_coords, r)
-        print_to_file(i)
-        time += dt
+    with ChargingBar('Steps', max=steps, suffix='%(percent)d%%') as bar:
+        for i in np.arange(1, steps+1):
+            calc_forces()
+            integrate()
+            list_coords = np.append(list_coords, r)
+            print_to_file(i)
+            time += dt
+            bar.next()
+    print('Done!')
 
 if __name__ == '__main__':
     main()
-    np.savetxt('crds.txt', list_coords, fmt='%f')
